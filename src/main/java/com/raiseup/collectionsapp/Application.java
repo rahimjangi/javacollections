12e3788335f43497cb172bd1ac0f9a9e21d975d5
pc <rahimjangi1982@gmail.com>
2022-09-19 20:48:38 +0200

    collections

---
+++ src/main/java/com/raiseup/collectionsapp/Application.java
@@ -0,0 +1,8 @@
+package com.raiseup.collectionsapp;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+//        Collections
+    }
+}
