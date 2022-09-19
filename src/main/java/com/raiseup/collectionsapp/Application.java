edac6c390bfdc58f768cb4072e637bc04a85cc5e
pc <rahimjangi1982@gmail.com>
2022-09-19 20:54:06 +0200

    init

--- src/main/java/com/raiseup/collectionsapp/Application.java
+++ src/main/java/com/raiseup/collectionsapp/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello world!");
 //        Collections
+        System.out.println("collections up and running...");
     }
 }
